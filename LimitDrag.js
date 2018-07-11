ea1ed9eff8070eb79d145c427982e75a91d9ec5a
wangstudyvc <wangstudyvc@163.com>
2018-07-11 21:25:47 +0800

    新增继承和命名空间的示例

---
+++ LimitDrag.js
@@ -0,0 +1,25 @@
+//继承Drag类
+function LimitDrag(id) {
+	Drag.call(this, id);	//继承属性
+}
+//继承方法
+for (var i in Drag.prototype) {
+	LimitDrag.prototype[i] = Drag.prototype[i];
+}
+
+//重写父类方法
+LimitDrag.prototype.fnMove = function (ev) {
+	var oEvent = ev || event;
+	var l = oEvent.clientX - this.disX;
+	var t = oEvent.clientY - this.disY;
+
+	if (l < 0) {
+		l = 0;
+	}
+	else if (l > document.documentElement.clientWidth - this.oDiv.offsetWidth) {
+		l = document.documentElement.clientWidth - this.oDiv.offsetWidth;
+	}
+
+	this.oDiv.style.left = l + 'px';
+	this.oDiv.style.top = t + 'px';
+};
